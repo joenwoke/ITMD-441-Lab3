c246a2b6624f11a45044f59ccac77ec09d5122c6
joenwoke <jnwoke@hawk.iit.edu>
2025-03-08 21:22:18 -0600

    Updated function to calculate avergae, minvalue, max and total numbers

--- jnwoke-lab3.js
+++ jnwoke-lab3.js
@@ -3,5 +3,8 @@
 
 //Exercise #1
 function minMaxAverage(numbers) {
-	
+	const totalNumbers = numbers.length;
+	const minValue = Math.min(...numbers);
+	const maxValue = Math.max(...numbers);
+	const average = numbers.reduce((acc, num) => acc + num, 0) /totalNumbers;
 }
