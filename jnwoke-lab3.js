e666817bf2394e12f179fad28d6e3b5e90ae2c9a
joenwoke <jnwoke@hawk.iit.edu>
2025-03-08 21:30:33 -0600

    Completed function to calculate and print output

--- jnwoke-lab3.js
+++ jnwoke-lab3.js
@@ -7,4 +7,8 @@ function minMaxAverage(numbers) {
 	const minValue = Math.min(...numbers);
 	const maxValue = Math.max(...numbers);
 	const average = numbers.reduce((acc, num) => acc + num, 0) /totalNumbers;
+	
+	const result = "Total Numbers: " + totalNumbers + ", Min Value: " + minValue + ", Max Value: " + maxValue + ", Average: " + average;
+	console.log(result);
 }
+
